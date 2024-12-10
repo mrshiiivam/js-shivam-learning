89ad52748e21d0ee9d38fde858eee85e34b7da30
SHIVAM TIWARI <115209071+mrshiiivam@users.noreply.github.com>
2024-12-10 07:13:21 +0000

    secondnew commit

--- 03-cont-variable/content01.js
+++ 03-cont-variable/content01.js
@@ -2,7 +2,12 @@ const accountId= 31292
 let accountEmail="st8968412@gmail.com"
 var accountPassword="1234"
 accountName="shivam"
+let accountState;
+// accountId= 2101641559001 "not possible to change const value"
 
-accountId= 2101641559001
+accountEmail="shivamt1699@gmail.com";
+accountPassword='212121';
+accountName="baroda";
 
 console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountName,accountState])
