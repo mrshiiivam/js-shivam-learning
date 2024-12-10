7339f2e47c1a53bc18dbdc48fc9bc925b49885f2
SHIVAM TIWARI <115209071+mrshiiivam@users.noreply.github.com>
2024-12-10 09:26:26 +0000

    converion commited

---
+++ 04-datatype/datatypeconversion.js
@@ -0,0 +1,49 @@
+let score=33;
+
+console.log(typeof score);
+console.log(typeof(score));  // this is also the same thing to define a datatype.
+
+let valueInNumber=Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// if there is used a number as a string
+
+let score1="33";
+
+console.log(typeof score1);
+console.log(typeof(score1));  // this is also the same thing to define a datatype.
+
+let valueInNumber1=Number(score1);
+
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+// if there is user a number with string as a string the convert it into the number so it will so Not a Number.
+
+let score2="33abc";
+
+console.log(typeof score2);
+console.log(typeof(score2));  // this is also the same thing to define a datatype.
+
+let valueInNumber2=Number(score2);
+
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+
+// same as we can do with string and boolean aslo
+
+let Name ="22shivam"
+
+let changeName=Number(Name);
+
+console.log(typeof changeName)
+console.log(changeName)
+
+let valueLoggedIn="1"
+// console.log(valueLoggedIN)
+let newvalueLoggedIn=Boolean(valueLoggedIn)
+console.log(newvalueLoggedIn)
+console.log(typeof newvalueLoggedIn)
