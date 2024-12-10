17c5efb31e703c8f5c0c9d952d36b86baaa35e56
SHIVAM TIWARI <115209071+mrshiiivam@users.noreply.github.com>
2024-12-10 08:44:19 +0000

    datatype new commit

---
+++ 04-datatype/03-datatype.js
@@ -0,0 +1,41 @@
+ "use strict"; // to start engine as a new vwrsion
+ 
+//  alert(3+3); 
+//this statement is not working in nodejs, it will work on browser.
+
+//  specing (;) is required to maintain readability of the code.
+
+console.log(3 
+    +3);  console.log("shivam tiwari")  //this code is right but the way it presenting is not good as per reading the code.
+
+// number datatype = 2 to power 53.
+let age=18
+
+// boolean datatype = true/false.
+let isLoggedIn=true;
+
+// biginit datatype = it use for big number.
+let num=12344000000000000000;
+
+// string datatype 
+// string str="shivam tiwari"
+let Name="shivamji"
+
+// null datatype =empaty value.
+let State=null
+
+// undefind datatype =it does not assign the value in the variable
+let accountArea;
+
+ console.log(age)
+ console.log(typeof 18)
+ console.log(isLoggedIn)
+ console.log(typeof true)
+ console.log(Name)
+ console.log(typeof Name)
+ 
+console.log(State)
+console.log(typeof State)
+
+console.log(typeof accountArea)
+ 
